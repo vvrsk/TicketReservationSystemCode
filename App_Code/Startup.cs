64ca76b0881f3c8406921aca6b6da37eaaca196c
mrinaldhawan <dhawan.mrinal@gmail.com>
2017-02-08 01:23:17 -0600

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketReservationSystem.Startup))]
+namespace TicketReservationSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
